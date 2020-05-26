55e9d8662ac48f4a6cd3bc437eebc5417dfe48cf
Jason Welsh <jasonbwelsh@gmail.com>
2020-05-25 23:39:53 -0400

    style guide tweak

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const URL = 'https://restcountries.eu/rest/v2/all';
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const response = await axios.get(URL);
+      console.log(response.data);
+      setCountries(response.data);
+    };
+
+    fetchCountries();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      jshfjkhfkjhk
+      <span>dgdgd</span>
     </div>
   );
 }
