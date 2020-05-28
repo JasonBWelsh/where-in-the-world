038a1ec4d126bdc5b5492d64729202d7e1e0669d
Jason Welsh <jasonbwelsh@gmail.com>
2020-05-28 13:33:29 -0400

    ErrorPage component added

--- src/components/CountriesPage/CountriesPage.js
+++ src/components/CountriesPage/CountriesPage.js
@@ -3,11 +3,13 @@ import { StyledCountriesPage } from './styles.js';
 import SearchForm from '../SearchForm/SearchForm.js';
 import CountriesList from '../CountriesList/CountriesList.js';
 import LoadingPage from '../LoadingPage/LoadingPage.js';
+import ErrorPage from '../ErrorPage/ErrorPage.js';
 
 function CountriesPage({ isLoading, isError }) {
   return (
     <StyledCountriesPage>
       <SearchForm />
+      {isError && <ErrorPage />}
       {isLoading ? <LoadingPage /> : <CountriesList />}
     </StyledCountriesPage>
   );
---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledErrorPage } from './styles.js';
+
+function ErrorPage() {
+  return (
+    <StyledErrorPage>
+      <h2>Something went wrong :(</h2>
+    </StyledErrorPage>
+  );
+}
+
+export default StyledErrorPage;
