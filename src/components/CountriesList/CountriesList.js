4c9bbf8549a62370a1fc27a52636b09c94213ebb
Jason Welsh <jasonbwelsh@gmail.com>
2020-06-05 20:09:31 -0400

    defensive commit befor eredux refactor. Pagination logic comeplete

--- src/components/CountriesList/CountriesList.js
+++ src/components/CountriesList/CountriesList.js
@@ -2,16 +2,20 @@ import React, { useContext } from 'react';
 import { StyledCountriesList } from './styles.js';
 import CountryPanel from '../CountryPanel/CountryPanel.js';
 import CountriesContext from '../../context/CountriesContext.js';
+import Pagination from '../Pagination/Pagination.js';
 
 const CountriesList = function () {
   const countries = useContext(CountriesContext);
 
   return (
-    <StyledCountriesList>
-      {countries.map((country) => (
-        <CountryPanel key={parseInt(country.numericCode)} country={country} />
-      ))}
-    </StyledCountriesList>
+    <>
+      <StyledCountriesList>
+        {countries.map((country) => (
+          <CountryPanel key={parseInt(country.numericCode)} country={country} />
+        ))}
+      </StyledCountriesList>
+      <Pagination />
+    </>
   );
 };
 
