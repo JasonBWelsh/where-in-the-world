ad5f7ee5e7689a32a5fe1b6edf8d05495b135438
Jason Welsh <jasonbwelsh@gmail.com>
2020-06-09 16:12:58 -0400

    CountryDetails getting country data from local storage to persist on reload

--- src/components/CountryDetails/CountryDetails.js
+++ src/components/CountryDetails/CountryDetails.js
@@ -1,25 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { StyledCountryDetails } from './styles.js';
 import { Link } from 'react-router-dom';
+import ErrorPage from '../ErrorPage/ErrorPage.js';
 
 function CountryDetails() {
   const [selectedCountry, setSelectedCountry] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
+    setIsLoading(true);
     const item = localStorage.getItem('selectedCountry');
-    setSelectedCountry(item);
+    setSelectedCountry(JSON.parse(item));
+    setIsLoading(false);
   }, []);
 
-  if (selectedCountry) {
-    const country = JSON.parse(selectedCountry);
-    console.log('DRD inside `CountryDetails` log `country`:::', country);
+  if (isLoading || !selectedCountry) {
+    return <ErrorPage />;
   }
 
-  return (
-    <StyledCountryDetails>
-      <h2>CountryDetails</h2>
-      <Link to="/">Back</Link>
-    </StyledCountryDetails>
-  );
+  if (selectedCountry) {
+    return (
+      <StyledCountryDetails>
+        <nav className="country-details__nav">
+          <Link to="/">Back</Link>
+        </nav>
+        <section className="country-info__container">
+          <div className="flag-container">
+            <img
+              src={selectedCountry.flag}
+              className="flag"
+              alt={`flag of ${selectedCountry.name}`}
+            />
+          </div>
+          <div className="data-container">data container</div>
+        </section>
+      </StyledCountryDetails>
+    );
+  }
 }
 
 export default CountryDetails;
